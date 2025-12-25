9ca9dcc641a93c93afc440f8bf72545e09878f4d
Siphamandla Malaza <malaza.siphamandla.david@gmail.com>
2025-12-25 23:21:34 +0200

    Added library to pull South African holidays

--- leave-planner.py
+++ leave-planner.py
@@ -1,5 +1,12 @@
 import calendar
+import holidays
+
 
 #year = input("Enter year")
 year = 2026
+south_africa_holidays = holidays.SouthAfrica(years=year)
+
+for date, name in sorted(south_africa_holidays.items()):
+    print(f"{date}: {name}")
+
 print(calendar.calendar(year))
